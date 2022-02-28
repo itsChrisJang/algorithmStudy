f537c365b6754a70bbaabbd4ebfdd13e9870098c
Chris Jang <62946867+itsChrisJang@users.noreply.github.com>
2022-02-28 21:57:59 +0900

    [백준]

---
+++ 백준/11726/TwoMulitipleN11726.java
@@ -0,0 +1,36 @@
+package self.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TwoMulitipleN11726 {
+
+    // 백준 11726번
+    // 점화식 : dp[n] = dp[n-1] + dp[n-2]; // (n >= 3)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = strToInt(br.readLine());
+
+        if(n == 2) {
+            System.out.println(2);
+        }else if(n == 1) {
+            System.out.println(1);
+
+        }
+        int[] np = new int[n];
+
+        np[0] = 1;
+        np[1] = 2;
+        for(int i = 3; i <= n; i++) {
+            np[i-1] = np[i-2] + np[i-3];
+        }
+        sb.append(np[n-1]%10007);
+        System.out.println(sb);
+        sb.setLength(0);
+    }
+
+    static int strToInt(String str) { return Integer.parseInt(str); }
+}
---
+++ 백준/9095/NumOneTwoThreeTotal9095.java
@@ -0,0 +1,30 @@
+package self.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumOneTwoThreeTotal9095 {
+
+    // 백준 9095번
+    // Dynamic Programming (동적 프로그래밍)
+    // 점화식 : dp[n] = dp[n-1] + dp[n-2] + dp[n-3]; // (n >= 4)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = stoi(br.readLine());
+        int dp[] = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i = 4; i < 11; i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        for(int i = 0; i < n; i++){
+            int t = stoi(br.readLine());
+            sb.append(dp[t] + "\n");
+        }
+        System.out.println(sb);
+    }
+    static int stoi(String str) {return Integer.parseInt(str);}
+}
