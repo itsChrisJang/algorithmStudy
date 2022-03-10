e9dfa1d1621b500a428a809784f799eebb64417e
Chris Jang <62946867+itsChrisJang@users.noreply.github.com>
2022-03-10 22:53:17 +0900

    [백준] N과 M (1)
    백트래킹

---
+++ baekjoon/bfsNdfs/NandM15649.java
@@ -0,0 +1,50 @@
+package self.baekjoon.bfsNdfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NandM15649 {
+
+    // 백준 15649번
+    // 백트래킹
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
---
+++ baekjoon/bfsNdfs/NandM15649GlobalVariable.java
@@ -0,0 +1,55 @@
+package self.baekjoon.bfsNdfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NandM15649GlobalVariable {
+
+    // 백준 15649번
+    // 백트래킹
+    public static int N;    // 정적변수로 변경
+    public static int M;    // 정적변수로 변경
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 정적변수 N과 M을 초기화해준다.
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        // 정적변수를 쓰면 되기 때문에 굳이 N과 M을 넘겨줄 필요 없다.
+        dfs(0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
