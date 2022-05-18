954e19417200aac98f03c369dec9b1d43688e146
Chris Jang <62946867+itsChrisJang@users.noreply.github.com>
2022-05-18 13:42:55 +0900

    [백준1946] 신입사원
    Comparator - compare로 오름차순 정렬하여 해결하였음.

---
+++ baekjoon/sort/NewRecruit1946_Comparator.java
@@ -0,0 +1,52 @@
+package self.baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class NewRecruit1946_Comparator {
+
+    // 백준 1946번
+    // 신입사원
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int N = 0;
+        int[][] caseArr = null;
+
+        StringTokenizer st;
+
+        for(int i=0; i<T; i++) {
+            N = Integer.parseInt(br.readLine());
+            caseArr = new int[N][2];
+
+            for(int j=0; j<N; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                caseArr[j][0] = Integer.parseInt(st.nextToken());
+                caseArr[j][1] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(caseArr, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    // 오름차순 정렬
+                    return o1[1] - o2[1];
+                }
+            });
+
+            int precious_rank = caseArr[0][0];
+            int cnt = 1;
+            for(int k=1; k<N; k++) {
+                if(precious_rank > caseArr[k][0]) {
+                    precious_rank = caseArr[k][0];
+                    cnt++;
+                }
+            }
+
+            System.out.println(cnt);
+        }
+    }
+}
